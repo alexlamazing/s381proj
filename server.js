9db403440461077952e3f835f7d24de61fcaa32e
amzfree <amzfree@gmail.com>
2015-12-06 21:12:13 +0800

    Delete extra code in GET /restaurant/

--- server.js
+++ server.js
@@ -99,12 +99,6 @@ app.get('/restaurant/', function(req,res) {
 				res.status(500).json(err);
 				throw err
 			}
-			if (results.length > 0) {
-				res.status(200).json(results);
-			}
-			else {
-				res.status(200).json({message: 'No matching document', restaurant_id: req.params.id});
-			}
 			db.close();
     	});
     });
