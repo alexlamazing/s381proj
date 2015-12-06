976fda483c560781844ee8b1e9e54a86ebe39c97
amzfree <amzfree@gmail.com>
2015-12-07 00:34:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.put('/restaurant_id/:id/grade', function(req,res) {
 	});
 });
 
-app.put('/:field/:value', function(req,res) {
+app.get('/:field/:value', function(req,res) {
 	var restaurantSchema = require('./models/restaurant');
 	mongoose.connect(mongodbURL);
 	var db = mongoose.connection;
@@ -142,23 +142,18 @@ app.put('/:field/:value', function(req,res) {
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
 		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
-		var rname = req.body.name;
-		var criteria = {"name":name};
 		var fieldName = req.params.field;
 		var fieldValue = req.params.value;
-		res.status(200).json({message: 'Update done', rname});
-		/*
-		Restaurant.update({fieldName:fieldValue},{$set:criteria},function(err){
+		Restaurant.find({fieldName:fieldValue},function(err, results){
 			if (err) {
 				console.log("Error: " + err.message);
 				res.write(err.message);
 			}
 			else {
 				db.close();
-				res.status(200).json({message: 'Update done'});
+				res.status(200).json(results);
 			}
 		});
-		*/
 	});
 });
 
